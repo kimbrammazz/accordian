d8dee04b2352fd4370740f5806b9d90918f44659
Kim Bramwell Mazzarella <kimbrammazz@gmail.com>
2024-03-15 10:28:02 +1100

    start javascript

---
+++ app.js
@@ -0,0 +1,11 @@
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach((btn) => {
+	console.log(btn);
+	btn.addEventListener("click", (e) => {
+		//traverse the dom to find the parent to attach the class
+		console.log(e.currentTarget.parentElement.parentElement);
+		const targetQuestion = e.currentTarget.parentElement.parentElement;
+		targetQuestion.classList.toggle("show-text");
+	});
+});
